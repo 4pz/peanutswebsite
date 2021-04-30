ffd4d04f79d1a7e918999284ec4ce9330669151d
Simple <vinayven01@gmail.com>
2021-04-30 15:45:47 -0700

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react"
 import "./style.css"
 import Header from "./compontents/Header";
-import Content from "./compontents/Content";
 
 function App() {
   return (
--- src/compontents/Header.js
+++ src/compontents/Header.js
@@ -1,14 +1,38 @@
 import React from "react"
+import axios from "axios";
 
 function Header() {
+
+    function encode() {
+        var value = document.getElementById("formvalue").value
+        axios.get("https://peanutsrestapi.herokuapp.com/encode?value=" + value)
+            .then(response => {
+                var encodedvalue = document.getElementById("encodedvalue")
+                encodedvalue.value = response.data
+            })
+            .catch(console.error)
+    }
+
+    function clear() {
+        var value = document.getElementById("formvalue")
+        var encodedvalue = document.getElementById("encodedvalue")
+        encodedvalue.value = ""
+        value.value = ""
+    }
+
     return (
-        <div>
-            <div className="container">
-                <header className="navtext">Peanuts Encoder</header>
-                <a href="#content"><div className="arrow bounce"></div></a>
+        <div className="container">
+            <div className="half">
+                <textarea id="formvalue" rows = "20" cols = "136" name = "value" >
+
+                </textarea>
+                <input id="formsubmit" type="submit" value="Encode" onClick={encode}></input>
             </div>
-            <div className="content">
-            
+            <div className="half">
+                <textarea id="encodedvalue" rows = "20" cols = "136" name = "value" >
+
+                </textarea>
+                <input id="formsubmit" type="submit" value="Clear" onClick={clear}></input>
             </div>
         </div>
     )
