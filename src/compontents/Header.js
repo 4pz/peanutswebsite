aa56c1077e74501666994853af706dc5c6d76221
Simple <vinayven01@gmail.com>
2021-04-30 00:30:31 -0700

    Initial commit

---
+++ src/compontents/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function Header() {
+    return (
+        <div>
+            <div className="container">
+                <header className="navtext">Peanuts Encoder</header>
+                <a href="#content"><div className="arrow bounce"></div></a>
+            </div>
+            <div className="content">
+            
+            </div>
+        </div>
+    )
+}
+
+export default Header
