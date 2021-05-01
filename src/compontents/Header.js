232107c77304b352b0ffe12718f1a945ff60b43e
Simple <vinayven01@gmail.com>
2021-05-01 00:56:18 -0700

    change

--- src/compontents/Header.js
+++ src/compontents/Header.js
@@ -13,6 +13,16 @@ function Header() {
             .catch(console.error)
     }
 
+    function decode() {
+        var value = document.getElementById("formvalue1").value
+        axios.get("https://peanutsrestapi.herokuapp.com/decode?value=" + value)
+            .then(response => {
+                var encodedvalue = document.getElementById("decodedvalue")
+                encodedvalue.value = response.data
+            })
+            .catch(console.error)
+    }
+
     function clear() {
         var value = document.getElementById("formvalue")
         var encodedvalue = document.getElementById("encodedvalue")
@@ -20,32 +30,44 @@ function Header() {
         value.value = ""
     }
 
+    function clear1() {
+        var value = document.getElementById("formvalue1")
+        var encodedvalue = document.getElementById("decodedvalue")
+        encodedvalue.value = ""
+        value.value = ""
+    }
+
     return (
         <div className="container">
-            <div className="half">
-                <textarea id="formvalue" rows = "20" cols = "136" name = "value" >
+            <div class="sec">
+                <div className="half">
+                    <textarea id="formvalue" rows = "20" cols = "136" name = "value" >
 
-                </textarea>
-                <input id="formsubmit" type="submit" value="Encode" onClick={encode}></input>
-            </div>
-            <div className="half">
-                <textarea id="encodedvalue" rows = "20" cols = "136" name = "value" >
+                    </textarea>
+                    <input id="formsubmit" type="submit" value="Encode" onClick={encode}></input>
+                </div>
+                <div className="half">
+                    <textarea id="encodedvalue" rows = "20" cols = "136" name = "value" >
 
-                </textarea>
-                <input id="formsubmit" type="submit" value="Clear" onClick={clear}></input>
+                    </textarea>
+                    <input id="formsubmit" type="submit" value="Clear" onClick={clear}></input>
+                </div>
             </div>
-            <div className="half">
-                <textarea id="formvalue" rows = "20" cols = "136" name = "value" >
+            <div class="sec">
+                <div className="half">
+                    <textarea id="formvalue1" rows = "20" cols = "136" name = "value" >
 
-                </textarea>
-                <input id="formsubmit" type="submit" value="Encode" onClick={encode}></input>
-            </div>
-            <div className="half">
-                <textarea id="encodedvalue" rows = "20" cols = "136" name = "value" >
+                    </textarea>
+                    <input id="formsubmit" type="submit" value="Decode" onClick={decode}></input>
+                </div>
+                <div className="half">
+                    <textarea id="decodedvalue" rows = "20" cols = "136" name = "value" >
 
-                </textarea>
-                <input id="formsubmit" type="submit" value="Clear" onClick={clear}></input>
+                    </textarea>
+                    <input id="formsubmit" type="submit" value="Clear" onClick={clear1}></input>
+                </div>
             </div>
+            <h1>Results for Decoding are not 100% Accurate</h1>
         </div>
     )
 }
